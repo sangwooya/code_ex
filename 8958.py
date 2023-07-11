905bab3a1832b6084972cb0fe72993d4cbc3bf9b
sangwoo <sangwoo6999@naver.com>
2023-07-11 12:20:44 +0900

    second commit

---
+++ 8958.py
@@ -0,0 +1,20 @@
+# N= int(input())
+# for _ in range(N):
+#     st=input()
+#     i=0
+#     j=0
+#     x=0
+#     while True:
+#         if st[j] == "O":
+#             i += 1 + i
+#             j += 1
+#             if st[j] == "X":
+#                 j += 1
+N = int(input())
+for _ in range(N):
+    st=input()
+    if
+
+
+
+
